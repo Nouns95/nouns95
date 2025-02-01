848ce29302c3d204bcb74354f79de7830ea75f99
wylin <iwylie959@gmail.com>
2025-01-31 20:46:55 -0600

    Nouns95 v2.0.0

--- src/config/files.ts
+++ src/config/files.ts
@@ -10,13 +10,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/traits-png',
     parentId: 'downloads',
     icon: '/icons/folder-16.png',
-    children: [],
-    stats: {
-      size: 0,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'directory'
-    }
+    children: []
   },
   {
     name: 'traits-svg',
@@ -24,13 +18,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/traits-svg',
     parentId: 'downloads',
     icon: '/icons/folder-16.png',
-    children: [],
-    stats: {
-      size: 0,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'directory'
-    }
+    children: []
   },
   {
     name: 'noggles',
@@ -38,13 +26,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/noggles',
     parentId: 'downloads',
     icon: '/icons/folder-16.png',
-    children: [],
-    stats: {
-      size: 0,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'directory'
-    }
+    children: []
   },
   // Files in traits-png folder
   {
@@ -53,13 +35,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/traits-png/trait1.png',
     parentId: 'traits-png',
     icon: '/icons/image-16.png',
-    downloadUrl: '/files/downloads/traits-png/trait1.png',
-    stats: {
-      size: 25600,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'image/png'
-    }
+    downloadUrl: '/files/downloads/traits-png/trait1.png'
   },
   {
     name: 'trait2.png',
@@ -67,13 +43,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/traits-png/trait2.png',
     parentId: 'traits-png',
     icon: '/icons/image-16.png',
-    downloadUrl: '/files/downloads/traits-png/trait2.png',
-    stats: {
-      size: 28800,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'image/png'
-    }
+    downloadUrl: '/files/downloads/traits-png/trait2.png'
   },
   // Files in traits-svg folder
   {
@@ -82,13 +52,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/traits-svg/trait1.svg',
     parentId: 'traits-svg',
     icon: '/icons/image-16.png',
-    downloadUrl: '/files/downloads/traits-svg/trait1.svg',
-    stats: {
-      size: 12800,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'image/svg+xml'
-    }
+    downloadUrl: '/files/downloads/traits-svg/trait1.svg'
   },
   {
     name: 'trait2.svg',
@@ -96,13 +60,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/traits-svg/trait2.svg',
     parentId: 'traits-svg',
     icon: '/icons/image-16.png',
-    downloadUrl: '/files/downloads/traits-svg/trait2.svg',
-    stats: {
-      size: 14400,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'image/svg+xml'
-    }
+    downloadUrl: '/files/downloads/traits-svg/trait2.svg'
   },
   // Files in noggles folder
   {
@@ -111,13 +69,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/noggles/noggle1.pdf',
     parentId: 'noggles',
     icon: '/icons/pdf-16.png',
-    downloadUrl: '/files/downloads/noggles/noggle1.pdf',
-    stats: {
-      size: 102400,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'application/pdf'
-    }
+    downloadUrl: '/files/downloads/noggles/noggle1.pdf'
   },
   {
     name: 'noggle2.pdf',
@@ -125,13 +77,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/noggles/noggle2.pdf',
     parentId: 'noggles',
     icon: '/icons/pdf-16.png',
-    downloadUrl: '/files/downloads/noggles/noggle2.pdf',
-    stats: {
-      size: 115200,
-      created: new Date('2024-01-29'),
-      modified: new Date('2024-01-29'),
-      type: 'application/pdf'
-    }
+    downloadUrl: '/files/downloads/noggles/noggle2.pdf'
   },
   // Original files
   {
@@ -140,13 +86,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Documents/readme.txt',
     parentId: 'documents',
     icon: '/icons/text-16.png',
-    downloadUrl: '/files/documents/readme.txt',
-    stats: {
-      size: 1024,
-      created: new Date('2024-01-28'),
-      modified: new Date('2024-01-28'),
-      type: 'text/plain'
-    }
+    downloadUrl: '/files/documents/readme.txt'
   },
   {
     name: 'sample.pdf',
@@ -154,13 +94,7 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Documents/sample.pdf',
     parentId: 'documents',
     icon: '/icons/pdf-16.png',
-    downloadUrl: '/files/documents/sample.pdf',
-    stats: {
-      size: 102400,
-      created: new Date('2024-01-28'),
-      modified: new Date('2024-01-28'),
-      type: 'application/pdf'
-    }
+    downloadUrl: '/files/documents/sample.pdf'
   },
   {
     name: 'image.jpg',
@@ -168,12 +102,6 @@ export const SAMPLE_FILES: SampleFileNode[] = [
     path: '/Downloads/image.jpg',
     parentId: 'downloads',
     icon: '/icons/image-16.png',
-    downloadUrl: '/files/downloads/image.jpg',
-    stats: {
-      size: 51200,
-      created: new Date('2024-01-28'),
-      modified: new Date('2024-01-28'),
-      type: 'image/jpeg'
-    }
+    downloadUrl: '/files/downloads/image.jpg'
   }
 ]; 
